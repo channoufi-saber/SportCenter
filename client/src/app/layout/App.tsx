636cff8147d1b4a0b99e6431bd0ef148876cdd72
channoufi-saber <saber.channoufi@esprit.tn>
2024-07-15 21:59:40 +0100

    Creating React Component

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,48 @@
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    }
+  })
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Container sx={{ paddingTop: "64px" }}>
+          <Catalog />
+        </Container>
+      </ThemeProvider>
+   
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
